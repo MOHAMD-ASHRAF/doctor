4a4c4bd1db29217a6b129e882514387c494b494f
DESKTOP-7C253GF\Eng MoHego <moashrafasd12@gmail.com>
2023-11-20 20:02:16 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:doctor/core/routing/app_router.dart';
+import 'package:flutter/material.dart';
+import 'package:doctor/doc_app.dart';
+void main() {
+  runApp( DocApp(appRouter: AppRouter(),));
+}
