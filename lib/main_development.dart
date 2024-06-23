87895eed56ae4e1c8ab11520563e9543e940c5fc
DESKTOP-7C253GF\Eng MoHego <moashrafasd12@gmail.com>
2024-06-23 21:01:20 +0300

    flavor

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -2,8 +2,7 @@ import 'package:doctor/core/di/dependency_injection.dart';
 import 'package:doctor/core/routing/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:doctor/doc_app.dart';
-import 'dart:io';
-void main() {
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  void main() {
   setupGetIt();
 
   runApp(DocApp(
