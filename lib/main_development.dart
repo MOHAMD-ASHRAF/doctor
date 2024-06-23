d60db0191a7123b625c29e6b46f02a77cc405317
DESKTOP-7C253GF\Eng MoHego <moashrafasd12@gmail.com>
2024-06-23 21:31:56 +0300

    flavor update

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -2,9 +2,11 @@ import 'package:doctor/core/di/dependency_injection.dart';
 import 'package:doctor/core/routing/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:doctor/doc_app.dart';
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  void main() {
-  setupGetIt();
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+void main() async{
+  await ScreenUtil.ensureScreenSize();
+  setupGetIt();
   runApp(DocApp(
     appRouter: AppRouter(),
   ));
--- lib/main_production.dart
+++ lib/main_production.dart
@@ -2,10 +2,11 @@ import 'package:doctor/core/di/dependency_injection.dart';
 import 'package:doctor/core/routing/app_router.dart';
 import 'package:flutter/material.dart';
 import 'package:doctor/doc_app.dart';
-import 'dart:io';
-void main() {
-  setupGetIt();
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+void main()async {
+  setupGetIt();
+  await ScreenUtil.ensureScreenSize();
   runApp(DocApp(
     appRouter: AppRouter(),
   ));
